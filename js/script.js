8e9a6381d32c755dd11c70664522e525c3ab4f21
Phill <phill.rodrigues@rocketmail.com>
2022-06-29 20:18:37 +0900

    updatecolor

--- js/script.js
+++ js/script.js
@@ -1,67 +1,69 @@
-$("#collapse2").hover(function () {
-    $(this).prev("a").css("color", "rgb(9, 255, 0)");
-    $("#itbrand").css("color", "rgb(9, 255, 0)");
-    $(".navbar").css("background-color", "#000000");
-},    function()  {
-    $(this).prev("a").css("color", "rgb(218, 218, 218)");
-    $("#itbrand").css("color", "rgb(171, 164, 235)");
-    $(".navbar").css("background-color", "rgb(18, 19, 24)");
-}
-);
-
-$(".legal").hover(function () {
-  $(this).css("color", "rgb(9, 255, 0)");
-  $("#itbrand").css("color", "rgb(9, 255, 0)");
-  $(".navbar").css("background-color", "#000000");
-},    function()  {
-  $(this).css("color", "rgb(218, 218, 218)");
-  $("#itbrand").css("color", "rgb(171, 164, 235)");
-  $(".navbar").css("background-color", "rgb(18, 19, 24)");
-}
-);
-
 $("#collapse1").hover(function () {
-  $(this).prev("a").css("color", "rgb(195, 0, 255)");
-  $("#itbrand").css("color", "rgb(195, 0, 255)");
+  $(this).prev("a").css("color", "rgb(180, 157, 229)");
+  $("#itbrand").css("color", "rgb(180, 157, 229)");
   $(".navbar").css("background-color", "#000000");
 
 },    function()  {
   $(this).prev("a").css("color", "rgb(218, 218, 218)");
   $("#itbrand").css("color", "rgb(171, 164, 235)");
-  $(".navbar").css("background-color", "rgb(18, 19, 24)");
+  $(".navbar").css("background-color", "rgb(40, 42, 53)");
 }
 );
 
 $(".it").hover(function () {
-  $(this).css("color", "rgb(195, 0, 255)");
-  $("#itbrand").css("color", "rgb(195, 0, 255)");
+  $(this).css("color", "rgb(180, 157, 229)");
+  $("#itbrand").css("color", "rgb(180, 157, 229)");
   $(".navbar").css("background-color", "#000000");
 },    function()  {
   $(this).css("color", "rgb(218, 218, 218)");
   $("#itbrand").css("color", "rgb(171, 164, 235)");
-  $(".navbar").css("background-color", "rgb(18, 19, 24)");
+  $(".navbar").css("background-color", "rgb(40, 42, 53)");
 }
 );
 
+$("#collapse2").hover(function () {
+    $(this).prev("a").css("color", "rgb(108, 221, 131)");
+    $("#itbrand").css("color", "rgb(108, 221, 131)");
+    $(".navbar").css("background-color", "#000000");
+},    function()  {
+    $(this).prev("a").css("color", "rgb(218, 218, 218)");
+    $("#itbrand").css("color", "rgb(171, 164, 235)");
+    $(".navbar").css("background-color", "rgb(40, 42, 53)");
+}
+);
+
+$(".legal").hover(function () {
+  $(this).css("color", "rgb(108, 221, 131)");
+  $("#itbrand").css("color", "rgb(108, 221, 131)");
+  $(".navbar").css("background-color", "#000000");
+},    function()  {
+  $(this).css("color", "rgb(218, 218, 218)");
+  $("#itbrand").css("color", "rgb(171, 164, 235)");
+  $(".navbar").css("background-color", "rgb(40, 42, 53)");
+}
+);
+
+
+
 $("#collapse3").hover(function () {
-  $(this).prev("a").css("color", "rgb(128, 74, 255)");
-  $("#itbrand").css("color", "rgb(128, 74, 255)");
+  $(this).prev("a").css("color", "rgb(231, 121, 184)");
+  $("#itbrand").css("color", "rgb(231, 121, 184)");
   $(".navbar").css("background-color", "#000000");
 },    function()  {
   $(this).prev("a").css("color", "rgb(218, 218, 218)");
   $("#itbrand").css("color", "rgb(218, 218, 218)");
-  $(".navbar").css("background-color", "rgb(18, 19, 24)");
+  $(".navbar").css("background-color", "rgb(40, 42, 53)");
 }
 );
 
 $(".interact").hover(function () {
-  $(this).css("color", "rgb(128, 74, 255)");
-  $("#itbrand").css("color", "rgb(128, 74, 255)");
+  $(this).css("color", "rgb(231, 121, 184)");
+  $("#itbrand").css("color", "rgb(231, 121, 184)");
   $(".navbar").css("background-color", "#000000");
 },    function()  {
   $(this).css("color", "rgb(218, 218, 218)");
   $("#itbrand").css("color", "rgb(218, 218, 218)");
-  $(".navbar").css("background-color", "rgb(18, 19, 24)");
+  $(".navbar").css("background-color", "rgb(40, 42, 53)");
 }
 );
 
