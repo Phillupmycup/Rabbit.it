a723d4b04e0bde1c7763e76a39cc38b15f1eb741
Phillip Rodrigues <phill.rodrigues@rocketmail.com>
2022-06-29 19:16:36 +0900

    please

---
+++ js/script.js
@@ -0,0 +1,67 @@
+$("#collapse2").hover(function () {
+    $(this).prev("a").css("color", "rgb(9, 255, 0)");
+    $("#itbrand").css("color", "rgb(9, 255, 0)");
+    $(".navbar").css("background-color", "#000000");
+},    function()  {
+    $(this).prev("a").css("color", "rgb(218, 218, 218)");
+    $("#itbrand").css("color", "rgb(171, 164, 235)");
+    $(".navbar").css("background-color", "rgb(18, 19, 24)");
+}
+);
+
+$(".legal").hover(function () {
+  $(this).css("color", "rgb(9, 255, 0)");
+  $("#itbrand").css("color", "rgb(9, 255, 0)");
+  $(".navbar").css("background-color", "#000000");
+},    function()  {
+  $(this).css("color", "rgb(218, 218, 218)");
+  $("#itbrand").css("color", "rgb(171, 164, 235)");
+  $(".navbar").css("background-color", "rgb(18, 19, 24)");
+}
+);
+
+$("#collapse1").hover(function () {
+  $(this).prev("a").css("color", "rgb(195, 0, 255)");
+  $("#itbrand").css("color", "rgb(195, 0, 255)");
+  $(".navbar").css("background-color", "#000000");
+
+},    function()  {
+  $(this).prev("a").css("color", "rgb(218, 218, 218)");
+  $("#itbrand").css("color", "rgb(171, 164, 235)");
+  $(".navbar").css("background-color", "rgb(18, 19, 24)");
+}
+);
+
+$(".it").hover(function () {
+  $(this).css("color", "rgb(195, 0, 255)");
+  $("#itbrand").css("color", "rgb(195, 0, 255)");
+  $(".navbar").css("background-color", "#000000");
+},    function()  {
+  $(this).css("color", "rgb(218, 218, 218)");
+  $("#itbrand").css("color", "rgb(171, 164, 235)");
+  $(".navbar").css("background-color", "rgb(18, 19, 24)");
+}
+);
+
+$("#collapse3").hover(function () {
+  $(this).prev("a").css("color", "rgb(128, 74, 255)");
+  $("#itbrand").css("color", "rgb(128, 74, 255)");
+  $(".navbar").css("background-color", "#000000");
+},    function()  {
+  $(this).prev("a").css("color", "rgb(218, 218, 218)");
+  $("#itbrand").css("color", "rgb(218, 218, 218)");
+  $(".navbar").css("background-color", "rgb(18, 19, 24)");
+}
+);
+
+$(".interact").hover(function () {
+  $(this).css("color", "rgb(128, 74, 255)");
+  $("#itbrand").css("color", "rgb(128, 74, 255)");
+  $(".navbar").css("background-color", "#000000");
+},    function()  {
+  $(this).css("color", "rgb(218, 218, 218)");
+  $("#itbrand").css("color", "rgb(218, 218, 218)");
+  $(".navbar").css("background-color", "rgb(18, 19, 24)");
+}
+);
+
